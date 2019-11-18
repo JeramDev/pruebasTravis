64d220bb8b1e5dd1274cca25c4914d5b70fa6394
Jesús Ramos <jesusramosdev@gmail.com>
2019-11-18 20:57:25 +0000

    Corregido error

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,7 +3,7 @@
 openssl aes-256-cbc -K $encrypted_76760c530019_key -iv $encrypted_76760c530019_iv
   -in travis.enc -out deploy-travis -d
 
-eval "$(ssh-agent -s)"
+eval $(ssh-agent -s)
 
 chmod 600 deploy-travis
 
